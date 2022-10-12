94a6bcd13ef000c16efcaad3dd8e98041993e324
ChrisEs146 <chris.es146@gmail.com>
2022-10-12 12:11:12 -0600

    Add: Proxy main structure

---
+++ index.js
@@ -0,0 +1,19 @@
+// Imports
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const weatherData = require("./routes/api");
+const errorHandler = require("./middleware/errorHandler");
+
+app.use(cors());
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+
+// Using routes
+app.use("/weather", weatherData);
+// settings port
+const PORT = process.env.PORT;
+// Adding errorHandler
+app.use(errorHandler);
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
